88814014ee761967a90072a040ee560294786f37
Radosław Mieckowski <rmieckowski@wp.pl>
2022-12-26 00:27:14 +0100

    add helloInternationalized method in HelloController.java, add .properties files to resources directory

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,10 +1,20 @@
 package com.example.demo.controllers;
 
 import com.example.demo.beans.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    private MessageSource messageSource;
+
+    public HelloController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     //@RequestMapping(method = RequestMethod.GET, path = "/hello")
     @GetMapping(path = "/hello") //wypisz string na ekran
@@ -21,6 +31,13 @@ public class HelloController {
     @GetMapping(path = "/hello-path-variable/{name}")
     public String helloPathVariable(@PathVariable String name) {
         return "Hello " + name;
+    }
+
+    @GetMapping(path = "/hello-internationalized")
+    public String helloInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(
+                "good.mornig.message", null, "default message", locale);
     }
 }
 
