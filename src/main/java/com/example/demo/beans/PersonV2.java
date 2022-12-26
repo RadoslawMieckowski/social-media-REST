0319a7c7f006940a7b8ff886f20ace4fc81f7c88
Radosław Mieckowski <rmieckowski@wp.pl>
2022-12-26 20:06:20 +0100

    add files

---
+++ src/main/java/com/example/demo/beans/PersonV2.java
@@ -0,0 +1,14 @@
+package com.example.demo.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PersonV2 {
+    private Name fullName;
+}
