ad8c30a85c7adeedc762ce95615ece29e769cb0f
Radosław Mieckowski <rmieckowski@wp.pl>
2022-12-26 20:50:13 +0100

    add EntityModel to getOneUser method in UserController.java, add hateoas dependency in pom.xml

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -4,6 +4,9 @@ import com.example.demo.beans.User;
 import com.example.demo.exceptions.UserNotExsitsException;
 import com.example.demo.service.UserDAOService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -12,24 +15,30 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class UserController {
     private UserDAOService service;
     public UserController(UserDAOService service) {
         this.service = service;
     }
+
     @GetMapping(path = "/users")
     public List<User> getUsers() {
         return service.getAllUsers();
     }
 
     @GetMapping(path = "/users/{id}")
-    public User getOneUser(@PathVariable int id) {
+    public EntityModel<User> getOneUser(@PathVariable int id) {
         User oneUser = service.getOneUser(id);
         if (oneUser == null) {
             throw new UserNotExsitsException("The user with an id = " + id + " was not found");
         }
-        return oneUser;
+        EntityModel<User> entityModel = EntityModel.of(oneUser);
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(methodOn(this.getClass()).getUsers());
+        entityModel.add(link.withRel("all-users"));     //adding link to the entity model
+        return entityModel;
     }
 
     @DeleteMapping(path = "/users/{id}")
