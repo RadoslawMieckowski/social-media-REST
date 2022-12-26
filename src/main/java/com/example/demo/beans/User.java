048898dbf7bc48ee1b7a8ad197f4bcefca434e7a
Radosław Mieckowski <rmieckowski@wp.pl>
2022-12-26 21:25:56 +0100

    add @JsonProperty annotations in User.java

--- src/main/java/com/example/demo/beans/User.java
+++ src/main/java/com/example/demo/beans/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.beans;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -12,9 +13,12 @@ import java.time.LocalDate;
 @Setter
 @Builder
 public class User {
+    @JsonProperty("user_id")
     private Integer id;
     @Size(min = 2, message = "user's name must be at least 2 letters long")
+    @JsonProperty("user_name")
     private String name;
     @Past(message = "user's birthDate must be in the past")
+    @JsonProperty("user_birth_date")
     private LocalDate birthDate;
 }
