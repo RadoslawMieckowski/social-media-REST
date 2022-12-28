8af492017332a9ce002f2d587f4c839c8804334a
Radosław Mieckowski <rmieckowski@wp.pl>
2022-12-28 14:49:25 +0100

    modify application.properties and User.java, add data.sql file

--- src/main/java/com/example/demo/beans/User.java
+++ src/main/java/com/example/demo/beans/User.java
@@ -1,6 +1,10 @@
 package com.example.demo.beans;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -12,13 +16,19 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Builder
+@Entity(name = "user_details")
 public class User {
-    @JsonProperty("user_id")
+    @JsonProperty("user_id") // doesn't relate to databases!
+    @Id                     // relates to the databases!
+    @GeneratedValue
     private Integer id;
+
     @Size(min = 2, message = "user's name must be at least 2 letters long")
     @JsonProperty("user_name")
     private String name;
+
     @Past(message = "user's birthDate must be in the past")
     @JsonProperty("user_birth_date")
+    @Column(name = "birth_date")    //necessary to fix an unknown bug (could not find column birthDate by the name)
     private LocalDate birthDate;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user_details(id, name, birth_date)
+values (100, 'Radek', current_date());
